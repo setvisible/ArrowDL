433715b49eb5c2094142e7f6116c56664ffdf232
SetVisible(0!=1) <setvisible@users.noreply.github.com>
2019-12-01 18:12:21 +0100

    [General] Improve Install/Uninstall scripts

--- web-extension/setup/install.bat
+++ web-extension/setup/install.bat
@@ -1,27 +1,47 @@
 @echo off
+:: Copyright 2019 Sébastien Vavassori. All rights reserved.
+:: Use of this source code is governed by a LGPL license that can be
+:: found in the LICENSE file.
 
 SET PATH=C:\Windows\System32;%PATH%
 
-rem Remove the final backslash with the ":n,m" substring syntax, here ":~0,-1"
+:: Remove the final backslash with the ":n,m" substring syntax, here ":~0,-1"
 SET currentPath=%~dp0
 SET INSTALL_PATH=%currentPath:~0,-1%
 
-ECHO .. Install directory: %INSTALL_PATH%
+ECHO *************************************************
+ECHO                  Down Right Now
+ECHO *************************************************
+ECHO.
+ECHO Install to: %INSTALL_PATH%
+ECHO.
 
-ECHO .. Writting to Chrome Registry
-ECHO .. Key: HKCU\Software\Google\Chrome\NativeMessagingHosts\com.setvisible.downrightnow
+ECHO.
+ECHO Writting to Chrome Registry...
+ECHO ---------------------------------
+ECHO Key: HKCU\Software\Google\Chrome\NativeMessagingHosts\com.setvisible.downrightnow
 REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.setvisible.downrightnow" /ve /t REG_SZ /d "%INSTALL_PATH%\launcher-manifest-chrome.json" /f
 
-ECHO .. Writting to Firefox Registry
-ECHO .. Key: HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\DownRightNow
+ECHO. 
+ECHO Writting to Firefox Registry...
+ECHO ---------------------------------
+ECHO Key: HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\DownRightNow
 REG ADD "HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\DownRightNow" /ve /t REG_SZ /d "%INSTALL_PATH%\launcher-manifest-firefox.json" /f
 
-ECHO .. Writting to Waterfox Registry
-ECHO .. Key: HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\DownRightNow
+ECHO.
+ECHO Writting to Waterfox Registry...
+ECHO ---------------------------------
+ECHO Key: HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\DownRightNow
 REG ADD "HKCU\SOFTWARE\Waterfox\NativeMessagingHosts\DownRightNow" /ve /t REG_SZ /d "%INSTALL_PATH%\launcher-manifest-firefox.json" /f
 
-ECHO .. Writting to Thunderbird Registry
-ECHO .. Key: HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\DownRightNow
+ECHO.
+ECHO Writting to Thunderbird Registry...
+ECHO ---------------------------------
+ECHO Key: HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\DownRightNow
 REG ADD "HKCU\SOFTWARE\Thunderbird\NativeMessagingHosts\DownRightNow" /ve /t REG_SZ /d "%INSTALL_PATH%\launcher-manifest-firefox.json" /f
 
-PAUSE
+ECHO.
+ECHO ^>^>^> Done! ^<^<^<
+ECHO.
+
+TIMEOUT /T 10
--- web-extension/setup/uninstall.bat
+++ web-extension/setup/uninstall.bat
@@ -1,19 +1,35 @@
 @echo off
+:: Copyright 2019 Sébastien Vavassori. All rights reserved.
+:: Use of this source code is governed by a LGPL license that can be
+:: found in the LICENSE file.
 
-echo .. Deleting Chrome Registry
+ECHO *************************************************
+ECHO                  Down Right Now
+ECHO *************************************************
+ECHO.
+
+ECHO.
+ECHO Deleting Chrome Registry...
+ECHO ---------------------------------
 REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.setvisible.downrightnow" /f
 
-echo .. Deleting Firefox Registry
+ECHO.
+ECHO Deleting Firefox Registry...
+ECHO ---------------------------------
 REG DELETE "HKCU\SOFTWARE\Mozilla\NativeMessagingHosts\DownRightNow" /f
 
-echo .. Deleting Waterfox Registry
+ECHO.
+ECHO Deleting Waterfox Registry...
+ECHO ---------------------------------
 REG DELETE "HKCU\SOFTWARE\Waterfox\NativeMessagingHosts\DownRightNow" /f
 
-echo .. Deleting Thunderbird Registry
+ECHO.
+ECHO Deleting Thunderbird Registry...
+ECHO ---------------------------------
 REG DELETE "HKCU\SOFTWARE\Thunderbird\NativeMessagingHosts\DownRightNow" /f
 
-echo.
-echo ^>^>^> Done! ^<^<^<
-echo.
-pause
+ECHO.
+ECHO ^>^>^> Done! ^<^<^<
+ECHO.
 
+TIMEOUT /T 10
