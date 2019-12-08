25e420cdaa87990fa464bf8c6969d6f9c86c5619
SetVisible(0!=1) <setvisible@users.noreply.github.com>
2019-12-08 21:45:19 +0100

    [WebExtension] Fix click action that should close the popup

--- web-extension/extension/chromium/popup.js
+++ web-extension/extension/chromium/popup.js
@@ -3,14 +3,17 @@
 document.getElementById("button-start").addEventListener('click', () => {
     // Call collectDOMandSendData() from 'background.js'
     chrome.extension.getBackgroundPage().collectDOMandSendData();
+    window.close();
 });
 
 document.getElementById("button-manager").addEventListener('click', () => { 
     var command = "[MANAGER]";
     chrome.extension.getBackgroundPage().sendData(command);
+    window.close();
 });
 
 document.getElementById("button-preference").addEventListener('click', () => { 
     var command = "[PREFS]";
     chrome.extension.getBackgroundPage().sendData(command);
+    window.close();
 });
