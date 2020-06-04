d4bb0e1f290ad14d57c8383603bddd5d948fb9a2
SetVisible(0!=1) <setvisible@users.noreply.github.com>
2020-06-04 18:57:44 +0200

    [Travis] Build Linux as .AppImage

---
+++ installer/unix/appimage/tools/wget.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+chmod a+x linuxdeploy-x86_64.AppImage
+
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool*.AppImage
+
